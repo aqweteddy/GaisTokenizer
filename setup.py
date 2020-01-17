b40b9616b43dec49ede217c5d7d947e96b7ba33b
aqweteddy <keddy940199@gmail.com>
2020-01-17 14:17:39 +0800

    rename

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 requirements = ['requests']
 
 setup(
-    name="GaisTokenizer",
+    name="gaisTokenizer",
     version="0.10",
     description="GaisTokenizer Service Python API",
     author="GAIS LAB & aqweteddy@github",
     url="",
     license="MIT",
     include_package_data=True,
-    packages=find_packages(include=["GaisTokenizer"]),
+    packages=find_packages(include=["gaisTokenizer"]),
     install_requires=requirements
 )
