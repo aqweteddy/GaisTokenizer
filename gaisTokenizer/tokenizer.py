f972066180ace4b0d2d2a37a312c430428d015b7
aqweteddy <keddy940199@gmail.com>
2020-02-15 18:04:26 +0800

    api token

--- gaisTokenizer/tokenizer.py
+++ gaisTokenizer/tokenizer.py
@@ -3,14 +3,17 @@ from typing import List, Tuple
 import requests
 from urllib.parse import urlencode
 
+
 class Tokenizer:
     BASE_URL = 'http://gaisdb.ccu.edu.tw:5721/api/segment?'
 
-    def __init__(self):
+    def __init__(self, token=None):
+        self.token = token
         self.tokenize('.')
-    
+
     def __send_request(self, text: str):
-        args = urlencode({'content': text})
+        args = urlencode({'token': self.token, 'content': text}
+                         if self.token else {'content': text})
         url = f'{self.BASE_URL}{args}'
 
         resp = requests.get(url)
@@ -18,8 +21,8 @@ class Tokenizer:
             print(f'Server Error {resp.status_code}')
 
         return resp.json()
-    
-    def tokenize(self, text: str, unk_token_idx: bool=False) -> List[str]:
+
+    def tokenize(self, text: str, unk_token_idx: bool = False) -> List[str]:
         """
         text: text to tokenize
         unk_index[bool]: if true, return word not in dictionary
@@ -28,19 +31,22 @@ class Tokenizer:
         result = result['recs']
 
         # remove * and process new word mark
-        unk_filter = lambda word: word[0] == '*' and len(word) > 1
-        if unk_token_idx:
-            unk_idx = [idx for idx, word in enumerate(result) if unk_filter(word)]
-        result = [word[1:] if unk_filter(word) else word for word in result]
+        def unk_filter(word): return word[0] == '*' and len(word) > 1
         
+        if unk_token_idx:
+            unk_idx = [idx for idx, word in enumerate(
+                result) if unk_filter(word)]
+        result = [word[1:] if unk_filter(word) else word for word in result]
+
         return (result, unk_idx) if unk_token_idx else result
-    
+
     def extract_keywords(self, text: str) -> List[str]:
         result = self.__send_request(text)['Keyterms']
         return [word.strip()for word in result.split(',') if word.strip()]
-    
+
     def get_words_keywords(self, text: str) -> Tuple[List[str]]:
         result = self.__send_request(text)
         words = result['recs']
-        keywords = [word.strip() for word in result['Keyterms'].split(',') if word.strip()]
+        keywords = [word.strip()
+                    for word in result['Keyterms'].split(',') if word.strip()]
         return words, keywords
